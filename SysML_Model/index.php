d55e5cdbd3a8498a042b8b8847adbc03f37f0890
Jan Frank <jan.frank@werktrieb.de>
2023-11-10 17:24:19 +0100

    Generate static html.

--- SysML_Model/index.php
+++ SysML_Model/index.php
@@ -1,13 +1,15 @@
 <?php
- $xml = new DOMDocument();
- $xml->load('ProST.uml');
-  
- $xsl = new DOMDocument();
- $xsl->load('ProST.xsl');
-  
- $xslt = new XsltProcessor();
- $xslt->importStylesheet($xsl);
-  
- $result = $xslt->transformToDoc($xml);
- echo $result->saveXML();
+	$xml = new DOMDocument();
+	$xml->load('ProST.uml');
+
+	$xsl = new DOMDocument();
+	$xsl->load('ProST.xsl');
+
+	$xslt = new XsltProcessor();
+	$xslt->importStylesheet($xsl);
+
+	$result = $xslt->transformToDoc($xml);
+	$html = $result->saveHTML();
+	echo $html;
+	file_put_contents('index.html', $html);
 ?>
